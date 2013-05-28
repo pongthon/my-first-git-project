5a5222e8388fca6e887c6c5e597b0f888d793524
pongthon <pongthon@outlook.com>
2013-05-28 16:15:52 +0700

    comment

---
+++ learn-ruby.rb
@@ -0,0 +1,48 @@
+# ruby comment
+class Person
+	def initialize(name, age, status)
+		@name = name
+		@age = age
+		@status = status
+	end
+
+	attr_accessor :name, :age, :status
+end
+
+module SpecialSkill
+	attr_reader : special_skill
+end
+
+class Doctor < Person
+	include SpecialSkill
+	def initialize(name, age, status, title = 'no title')
+		super(name, age, status)
+		raise "Invalid title" unless ["M.D", "Surgeon", "no-title"].include?(title)
+		@title = title
+		@special_skill = "heal human"
+	end
+
+	attr_accessor :title
+
+	def skills
+		if @title == "M.D"
+			"Medicine Doctr"
+		elsif @title == "Surgeon"
+			"Surgeon Doctor"
+		else
+			"Fake Doctor"
+		end
+	end
+end
+hoctor = Doctor.new("Doc", 60, "alive", "M.D")
+puts doctor.skills
+#doctor = Doctor.new("Doc", 60, "alive", "d.D")
+
+class Engineer < Person
+	include SpecialSkill
+	def initialize(name, age, status, special_skill)
+		super(name, age, status)
+		@special_skill = special_skill
+	end
+end
+
