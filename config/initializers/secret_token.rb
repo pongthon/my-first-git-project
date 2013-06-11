8497c6e751e6a2fa5c418b5c81eabf938cf6158b
pongthon <pongthon@outlook.com>
2013-06-11 13:10:52 +0700

    add rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votes::Application.config.secret_token = 'aa597675c781062770640685f17ebc5f3c1401d3e6a53692ad77e70cb563cf8697a33d58caad36355f973b2d71cfdd1717d62b6ea39cf6769362065f43838ab2'
